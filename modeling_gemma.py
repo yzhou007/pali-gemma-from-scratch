b3f81ee0ceafb265e795d5ad0df94cbe4862ef45
Yang Zhou <yzhou258@gmail.com>
2026-03-16 20:52:24 +0000

    add inference and launch files

--- modeling_gemma.py
+++ modeling_gemma.py
@@ -443,8 +443,7 @@ class PaliGemmaForConditionalGeneration(nn.Module):
         self.multi_modal_projector = PaliGemmaMultiModalProjector(config)
         self.vocab_size = config.vocab_size
 
-        language_model = GemmaForCausalLM(config.text_config)
-        self.language_model = language_model
+        self.language_model = GemmaForCausalLM(config.text_config)
 
         self.pad_token_id = self.config.pad_token_id if self.config.pad_token_id is not None else -1
 
